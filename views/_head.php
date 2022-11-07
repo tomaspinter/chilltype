f6b143598163e6e9d399ea789ce5d825571334c9
tomas <tomas.pinter@gmail.com>
2022-11-07 22:10:18 +0100

    added favicons

--- views/_head.php
+++ views/_head.php
@@ -6,6 +6,9 @@
     <meta charset="UTF-8">
     <script src=../texts.js></script>
     <script src=../chilltype.js></script>
+    <link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="favicons/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png">
 </head>
 <body>
     <div id="content">
