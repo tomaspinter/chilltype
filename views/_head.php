cd169b96f631d65ab4d1bec1421c91568967898d
tomas <tomas.pinter@gmail.com>
2022-11-08 21:14:35 +0100

    added additional meta tags

--- views/_head.php
+++ views/_head.php
@@ -1,9 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Chilltype</title>
+    <title>Chilltyping</title>
     <link rel="stylesheet" href="../chilltype.css">
     <meta charset="UTF-8">
+    <meta name="viewport" content = "width=device-width, initial-scale=1">
+    <meta name="keywords" content="typing, learn to type, typing game, retype text, learning typing, relaxing typing">
+    <meta name="description" content="Chill and relax while re-typing interesting facts. Review your stats at the end to see how well you did. Likewise, if you want to improve your typing skills, this is the no-stress zone for you.">
+    <meta name="author" content="Tomas Pinter">
     <script src=../texts.js></script>
     <script src=../chilltype.js></script>
     <link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png">
